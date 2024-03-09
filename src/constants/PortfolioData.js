a16c1bbcf9d4d897220e7a96ae0c881ac3c4e62c
Rashad Alakbarov <rashad.alakbarov.777@gmail.com>
2024-03-09 22:33:50 +0400

    update portfolio

--- src/constants/PortfolioData.js
+++ src/constants/PortfolioData.js
@@ -1,52 +1,20 @@
-import Work1 from "../assets/projects/work1.jpg";
-import Work2 from "../assets/projects/work2.jpg";
-import Work3 from "../assets/projects/work3.jpg";
-import Work4 from "../assets/projects/work4.jpg";
-import Work5 from "../assets/projects/work5.jpg";
 import racorb_home from "../assets/projects/racorb_home.png";
+import ecora_home from "../assets/projects/ecora_home.png";
 
 export const projectsData = [
   {
     id: 1,
-    image: Work1,
-    title: "Web design",
+    image: racorb_home,
+    title: "Racorb Technology",
     category: "web",
     link: "https://racorb.netlify.app/",
   },
   {
     id: 2,
-    image: Work2,
-    title: "App movil",
-    category: "app",
-    link: "https://racorb.netlify.app/",
-  },
-  {
-    id: 3,
-    image: Work3,
-    title: "Brand design",
-    category: "design",
-    link: "https://racorb.netlify.app/",
-  },
-  {
-    id: 4,
-    image: Work4,
-    title: "App movil",
-    category: "app",
-    link: "https://racorb.netlify.app/",
-  },
-  {
-    id: 5,
-    image: Work5,
-    title: "Web design",
+    image: ecora_home,
+    title: "Ecora Boutique",
     category: "web",
-    link: "https://racorb.netlify.app/",
-  },
-  {
-    id: 6,
-    image: racorb_home,
-    title: "Racorb Technology",
-    category: "web",
-    link: "https://racorb.netlify.app/",
+    link: "https://ecora-online-boutique.netlify.app/",
   },
 ];
 
