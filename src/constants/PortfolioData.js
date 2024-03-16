e7cc94bdb98b43d1a607dc835d5c06681c869429
Rashad Alakbarov <rashad.alakbarov.777@gmail.com>
2024-03-16 22:27:27 +0400

    update portfolio

--- src/constants/PortfolioData.js
+++ src/constants/PortfolioData.js
@@ -1,5 +1,6 @@
 import racorb_home from "../assets/projects/racorb_home.png";
 import ecora_home from "../assets/projects/ecora_home.png";
+import guiz_time from "../assets/projects/guiz-time.png";
 
 export const projectsData = [
   {
@@ -15,6 +16,13 @@ export const projectsData = [
     title: "Ecora Boutique",
     category: "web",
     link: "https://ecora-online-boutique.netlify.app/",
+  },
+  {
+    id: 3,
+    image: guiz_time,
+    title: "Guiz Time",
+    category: "web",
+    link: "https://guiz-time.netlify.app/",
   },
 ];
 
